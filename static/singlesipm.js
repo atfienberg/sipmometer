fb1f84be84cfdf1af71aa1a686c3a70153a81cb3
Aaron Fienberg <atfienberg@aarons-mbp.dhcp.fnal.gov>
2016-11-14 13:40:11 -0600

    added plotly code for single sipm

--- static/singlesipm.js
+++ static/singlesipm.js
@@ -4,39 +4,61 @@ $(document).ready(function() {
     var sipmNum = parseInt($('#sipmnum').text());
 
     (function askForPlot() {
-	socket.emit('temp plot', {'num' : sipmNum, 'hours': $('#howManyHours').val()});
-	setTimeout(askForPlot, 20000);
+        socket.emit('temp plot', { 'num': sipmNum, 'hours': $('#howManyHours').val() });
+        setTimeout(askForPlot, 20000);
     })();
 
     (function askForGain() {
-	socket.emit('single gain', {"num" : sipmNum});
-	setTimeout(askForGain, 10000);
+        socket.emit('single gain', { "num": sipmNum });
+        setTimeout(askForGain, 10000);
     })();
 
-    var traceplot = document.getElementById('plot');
-    
-    socket.on('plot ready', function(msg) {
-	if (msg.num == sipmNum){
+    function makeHorizontalLine(y) {
+        return {
+            type: 'line',
+            xref: 'paper',
+            yref: 'y',
+            x0: 0,
+            y0: y,
+            x1: 1,
+            y1: y,
+            line: { color: 'red' }
+        };
+    }
 
-	}
+    var plot = document.getElementById('plot');
+    Plotly.newPlot('plot', [
+        { y: [] }
+    ], {
+        titlefont: { size: 20 },
+        yaxis: { title: 'temperature' },
+    });
+    socket.on('plot ready', function(msg) {
+        if (msg.num == sipmNum) {
+            Plotly.deleteTraces(plot, 0);
+            Plotly.addTraces(plot, { x: data.x, y: data.y, type: 'line' });
+
+            var newlayout = { shapes: [data.plus, data.minus].map(makeHorizontalLine) };
+            Plotly.relayout(newlayout);
+        }
     });
 
     socket.on('sipm gain', function(msg) {
-	if (msg.num == sipmNum){
-	    $('#gainSetting').text(msg.gain);
-	}
+        if (msg.num == sipmNum) {
+            $('#gainSetting').text(msg.gain);
+        }
     });
 
     $('#howManyHours').keydown(function(e) {
-	if (e.which == 13) {
-	    socket.emit('temp plot', {'num' : sipmNum, 'hours': $('#howManyHours').val()});	
-	}
+        if (e.which == 13) {
+            socket.emit('temp plot', { 'num': sipmNum, 'hours': $('#howManyHours').val() });
+        }
     });
 
     $('#newSetting').keydown(function(e) {
-	if (e.which == 13) {
-	    socket.emit('set gain', {'num' : sipmNum, 'new_gain' : $('#newSetting').val()});
-	    $('#newSetting').val('');
-	}
+        if (e.which == 13) {
+            socket.emit('set gain', { 'num': sipmNum, 'new_gain': $('#newSetting').val() });
+            $('#newSetting').val('');
+        }
     });
 });
